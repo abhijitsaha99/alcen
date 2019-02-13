43491ceffcf36096ec6e9ffecbd384b1177eb66e
mahesdwivedi <mahesdwivedi@gmail.com>
2019-02-13 21:05:08 +0530

    code cleaning

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,24 @@ var session;
 const news = require('../models/notice')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {});
+router.get('/', function(req, res, next) {
+  var query = news.find({})
+  query.select('title body');
+  query.exec((err,data) => {
+    console.log(data);
+    res.render('index', {"datas": data});
+  })
+  });
+
 router.get('/ab1', function(req, res, next) {
   res.render("aboutus1");
-});
-router.get('/ab2', function(req, res, next) {
-  res.render('aboutus2');
-});
-router.get('/ab3', function(req, res, next) {
-  res.render('aboutus3');
-});
-router.get('/ab4', function(req, res, next) {
-  res.render('aboutus4');
-});
-router.get('/ab5', function(req, res, next) {
-  res.render('aboutus5');
 });
 router.get('/products', function(req, res, next) {
   res.render('products');
 });
 router.get('/details/:title', function(req, res, next) {
-  var content=req.params.title;
-  var query= news.find({title:content});
+  var d=req.params.title;
+  var query= news.find({title:d});
   query.select('title body');
   query.exec((err,data) => {
   console.log(data);
@@ -34,7 +30,7 @@ router.get('/details/:title', function(req, res, next) {
   });
   router.get('/login', function(req, res, next) {
   var flag=0;
-  res.render('form', {
+  res.render('login', {
     title: 'form',
     success: false,
     errors: req.session.errors,
@@ -72,6 +68,7 @@ router.post('/enter', function(req, res) {
     .then(res.redirect('/tst'))
     .catch((err) => console.log(err))
 });
+
 router.get('/tst', function(req, res, next) {
   var query = news.find({})
   query.select('title body');
